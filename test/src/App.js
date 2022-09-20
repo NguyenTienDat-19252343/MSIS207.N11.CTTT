150b5883900cd6e1f4144f4500f184c26f041c35
NguyenTienDat-19252343 <19521343@gm.uit.edu.vn>
2022-09-20 18:18:39 +0700

    Update App.js

--- test/src/App.js
+++ test/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=BhUthi9HGjg&list=RDBhUthi9HGjg&start_radio=1"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nguyen Tien Dat link
         </a>
       </header>
     </div>
